e2e2cf60bb16755003b33e633a985fe1fce85285
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-11-13 14:42:00 +0100

    Display weather data based on user input

--- weather.js
+++ weather.js
@@ -31,6 +31,38 @@ const processData = (data) => {
   return weatherData;
 };
 
+const displayWeather = (weatherData) => {
+  const {
+    name, region, country, temp_c, text, icon,
+  } = weatherData;
+  const weatherDisplay = document.createElement('div');
+  weatherDisplay.setAttribute('id', 'weather-display');
+
+  const location = document.createElement('h2');
+  location.textContent = `${name}, ${region}, ${country}`;
+
+  const temperature = document.createElement('h3');
+  temperature.textContent = `${temp_c}°C`;
+
+  const condition = document.createElement('h3');
+  condition.textContent = text;
+
+  const weatherIcon = document.createElement('img');
+  weatherIcon.setAttribute('src', `https:${icon}`);
+  weatherIcon.setAttribute('alt', 'weather icon');
+
+  weatherDisplay.appendChild(location);
+  weatherDisplay.appendChild(temperature);
+  weatherDisplay.appendChild(condition);
+  weatherDisplay.appendChild(weatherIcon);
+
+  if (weatherContainer.lastChild.id === 'weather-display') {
+    weatherContainer.removeChild(weatherContainer.lastChild);
+  }
+
+  weatherContainer.appendChild(weatherDisplay);
+};
+
 const fetchWeather = async () => {
   const city = searchBar.value;
   const url = `http://api.weatherapi.com/v1/current.json?key=29c39c47ff36404ba9d124845231211&q=${city}`;
@@ -40,6 +72,7 @@ const fetchWeather = async () => {
   console.log(data);
   const weatherData = processData(data);
   console.log(weatherData);
+  displayWeather(weatherData);
 };
 
 searchButton.addEventListener('click', () => {
